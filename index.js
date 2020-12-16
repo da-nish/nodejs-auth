aab16f3f1cbbc21a9c09b8f69dd39f08902dd890
danish <danishlnct@gmail.com>
2020-12-17 00:16:37 +0530

    storing session on db

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const path = require('path')
 const mongooes = require('mongoose')
 const app = express();
 const session = require('express-session')
-
+const mongodb_session = require('connect-mongodb-session')(session)
 
 //setting default template engine and template folder name
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 
+const db_url = 'mongodb+srv://root1:rootroot@cluster0.sdkrx.mongodb.net/userlogin' //db name userlogin
 
 const userRoute = require('./router/user_route')
 
@@ -19,7 +20,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 //static file serve: css, js
 
 
-app.use(session({secret:'secrect string', saveUninitialized: true,resave: false, cookie: { maxAge: 60000 }}))
+const session_store = new mongodb_session({uri:db_url, collection: 'session_data'})//storing sesion in mongodb
+
+app.use(session({secret:'secrect string', store:session_store, saveUninitialized: true,resave: false, cookie: { maxAge: 60000 }}))
 //ROUTER
 app.use(userRoute)
 app.use((req,res,next)=>{
@@ -27,8 +30,6 @@ app.use((req,res,next)=>{
 })
 
 
-
-const db_url = 'mongodb+srv://root1:rootroot@cluster0.sdkrx.mongodb.net/userlogin' //db name userlogin
 mongooes.connect(db_url,  {useNewUrlParser: true, useUnifiedTopology: true})
 .then(
     result => {
